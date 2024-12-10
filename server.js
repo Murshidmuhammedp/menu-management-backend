18850ebd1bcc3785434bb9fd6bcda7089035b195
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-12-10 11:06:20 +0530

    cors config

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ import cors from 'cors'
 const PORT = process.env.PORT || 5678;
 
 const app = express();
-
 dotenv.config();
-
-app.use(cors({
-    origin: "https://menu-management-frontend-blush.vercel.app/",
-    credentials: true
-}));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors({
+    origin: "https://menu-management-frontend-blush.vercel.app/",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 app.use('/api/menus', menuRouter);
 app.use('/api/menu-items', menuItemRouter);
